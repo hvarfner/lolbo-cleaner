d712362168128c69fda627536d750efc18e821b3
hvarfner <carl.hvarfner@live.se>
2024-04-17 22:03:04 +0200

    Added submit scripts, container recipe and paths script

---
+++ selfies_paths.sh
@@ -0,0 +1,4 @@
+export VAE_PATH="${PWD}/selfies_vae/state_dict/SELFIES-VAE-state-dict.pt"
+export VAE_LOAD="load_selfies_vae"
+export INIT_PATH="${PWD}/selfies_vae/init/"
+export SAVE_PATH="/mimer/NOBACKUP/groups/molecule_bo"
